d83e83c606e8fc5f1be655ef04acbd6ac44bce09
Zoey <joyivan6@gmail.com>
2023-12-27 17:23:29 +0700

    add case1 and case2

---
+++ case1/solution1.js
@@ -0,0 +1,86 @@
+const fruits = [
+    {
+    fruitId: 1,
+    fruitName: 'Apel',
+    fruitType: 'IMPORT',
+    stock: 10
+    },
+    {
+    fruitId: 2,
+    fruitName: 'Kurma',
+    fruitType: 'IMPORT',
+    stock: 20
+    },
+    {
+    fruitId: 3,
+    fruitName: 'apel',
+    fruitType: 'IMPORT',
+    stock: 50
+    },
+    {
+    fruitId: 4,
+    fruitName: 'Manggis',
+    fruitType: 'LOCAL',
+    stock: 100
+    },
+    {
+    fruitId: 5,
+    fruitName: 'Jeruk Bali',
+    fruitType: 'LOCAL',
+    stock: 10
+    },
+    {
+    fruitId: 5,
+    fruitName: 'KURMA',
+    fruitType: 'IMPORT',
+    stock: 20
+    },
+    {
+    fruitId: 5,
+    fruitName: 'Salak',
+    fruitType: 'LOCAL',
+    stock: 150
+    }
+]
+
+function removeDuplicates(arr) {
+    let unique= []
+    arr.forEach(element => {
+        if (!unique.includes(element.toLowerCase())) {
+            unique.push(element.toLowerCase())
+        }
+    })
+    return unique
+}
+
+let fruitList= fruits.map((f) => f.fruitName)
+//1
+console.log("No 1")
+console.log(`Daftar nama buah: ${removeDuplicates(fruitList)}`)
+//Jawaban
+// Daftar nama buah: apel,kurma,manggis,jeruk bali,salak
+
+//2
+let localBag = fruits.filter((f) => f.fruitType === 'LOCAL').map((f) => f.fruitName)
+let importBag = fruits.filter((f) => f.fruitType === 'IMPORT').map((f) => f.fruitName)
+console.log("No 2")
+console.log("Jumlah wadah yang dibutuhkan: 2")
+console.log(`Daftar buah yang ada pada kantong lokal: ${removeDuplicates(localBag)}`)
+console.log(`Daftar buah yang ada pada kantong import: ${removeDuplicates(importBag)}`)
+//Jawaban
+//Daftar buah yang ada pada kantong lokal: manggis,jeruk bali,salak
+//Daftar buah yang ada pada kantong import: apel,kurma
+
+//3
+let totalLocal = removeDuplicates(localBag).length
+let totalImport = removeDuplicates(importBag).length
+console.log("No 3")
+console.log(`Total buah pada kantong lokal: ${totalLocal}`)
+console.log(`Total buah pada kantong import: ${totalImport}`)
+//Jawaban
+//Total buah pada kantong lokal: 3
+//Total buah pada kantong import: 2
+
+
+//4
+console.log("Soal ini menerapkan manipulasi array dari filtering, mapping")
---
+++ case2/solution2.js
@@ -0,0 +1,59 @@
+const comments = [
+    {
+      commentId: 1,
+      commentContent: 'Hai',
+      replies: [
+        {
+          commentId: 11,
+          commentContent: 'Hai juga',
+          replies: [
+            {
+              commentId: 111,
+              commentContent: 'Haai juga hai jugaa',
+            },
+            {
+              commentId: 112,
+              commentContent: 'Haai juga hai jugaa',
+            },
+          ],
+        },
+        {
+          commentId: 12,
+          commentContent: 'Hai juga',
+          replies: [
+            {
+              commentId: 121,
+              commentContent: 'Haai juga hai jugaa',
+            },
+          ],
+        },
+      ],
+    },
+    {
+      commentId: 2,
+      commentContent: 'Halooo',
+    },
+  ]
+
+  function countTotalComments(commentsArray) {
+    let totalComments = 0
+  
+    function countRecursive(comment) {
+      totalComments += 1
+  
+      if (comment.replies && comment.replies.length > 0) {
+        comment.replies.forEach((reply) => {
+          countRecursive(reply)
+        })
+      }
+    }
+  
+    commentsArray.forEach((comment) => {
+      countRecursive(comment)
+    })
+  
+    return totalComments
+  }
+
+
+  console.log(`Total komentar: ${countTotalComments(comments)}`)
